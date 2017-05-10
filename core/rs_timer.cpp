f70704fc50893a82f607f841016afb31da6b06d8
kevin.wen <listream@126.com>
2017-05-10 22:11:17 +0800

    add timer support
    Signed-off-by: kevin.wen <listream@126.com>

---
+++ core/rs_timer.cpp
@@ -0,0 +1,140 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016-2017 RabbitStreamer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "rs_timer.h"
+#include "rs_error_code.h"
+#include "rs_core_utility.h"
+#include <st.h>
+#include <assert.h>
+#include <algorithm>
+
+#define SYS_TIMER_LOOP_INTERVAL 500 //sleep for 500ms
+
+RsTimer* RsTimer::p = new RsTimer;
+RsTimer* RsTimer::instance()
+{
+    return p;
+}
+
+RsTimer::RsTimer()
+{
+    last_thread_time = 0;
+    thread_start_flag = false;
+}
+
+RsTimer::~RsTimer()
+{
+
+}
+
+int RsTimer::end_loop()
+{
+    int ret = ERROR_SUCCESS;
+
+    return ret;
+}
+
+int RsTimer::thread_stop()
+{
+    int ret = ERROR_SUCCESS;
+
+    return ret;
+}
+
+int RsTimer::thread_start()
+{
+    int ret = ERROR_SUCCESS;
+
+    return ret;
+}
+
+int RsTimer::before_loop()
+{
+    int ret = ERROR_SUCCESS;
+
+    return ret;
+}
+
+int RsTimer::loop()
+{
+    int ret = ERROR_SUCCESS;
+    check_timeout();
+    st_usleep(SYS_TIMER_LOOP_INTERVAL * 1000);
+    return ret;
+}
+
+void RsTimer::add_timer(int64_t timeout, int64_t timerid, ITimerHandler* callback)
+{
+    assert(callback!=NULL);
+    if(!thread_start_flag)
+    {
+        run("RsTimer");
+        thread_start_flag = true;
+    }
+    timer_item item;
+    item.timeout = timeout;
+    item.timerid = timerid;
+    item.callback = callback;
+    item.last_signal_time = 0;
+    timer_vector.push_back(item);
+}
+
+void RsTimer::delete_timer(int64_t timeout, int64_t timerid, ITimerHandler* callback)
+{
+    timer_item item;
+    item.timeout = timeout;
+    item.timerid = timerid;
+    item.callback = callback;
+    timer_vector.erase(remove(timer_vector.begin(), timer_vector.end(), item));
+}
+
+void RsTimer::check_timeout()
+{
+    int64_t curr = get_system_time_ms();
+
+    vector<timer_item>::iterator iter = timer_vector.begin();
+    //init the first signal time
+    if(last_thread_time == 0)
+    {
+        last_thread_time = curr;
+        for(; iter < timer_vector.end(); iter++)
+        {
+            iter->last_signal_time = curr;
+        }
+        return;
+    }
+    int64_t delta = 0;
+    //iterate the vector to trigger the timer's callback
+    iter = timer_vector.begin();
+    for(; iter < timer_vector.end(); iter++)
+    {
+       int64_t timeout = iter->timeout;
+       delta = curr-iter->last_signal_time;
+       if(delta >= timeout)
+       {
+           (iter->callback)->handle_timeout(iter->timerid);
+           iter->last_signal_time = curr;
+       }
+    }
+    return;
+}
---
+++ core/rs_timer.h
@@ -0,0 +1,90 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016-2017 RabbitStreamer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef CORE_RS_TIMER_H_
+#define CORE_RS_TIMER_H_
+#include <vector>
+#include <stdint.h>
+#include "rs_thread.h"
+
+using namespace std;
+
+class ITimerHandler
+{
+public:
+    ITimerHandler()
+    {
+    }
+    virtual ~ITimerHandler()
+    {
+    }
+
+public:
+    virtual int handle_timeout(int64_t timerid) = 0;
+};
+
+typedef struct timer_item
+{
+    int64_t timeout;
+    int64_t timerid;
+    int64_t last_signal_time;
+    ITimerHandler* callback;
+    bool operator==(const timer_item& rhs)
+    {
+        if((callback==rhs.callback)&&(timeout==rhs.timeout)
+            &&(timerid==rhs.timerid))
+            return true;
+        return false;
+    }
+}timer_item;
+
+class RsTimer : public RsThread
+{
+private:
+    static RsTimer* p;
+public:
+    RsTimer();
+    virtual ~RsTimer();
+public:
+    //for single instance
+    static RsTimer* instance();
+public:
+    //implement rs_thread's virtual function
+    virtual int thread_start();
+    virtual int before_loop();
+    virtual int loop();
+    virtual int end_loop();
+    virtual int thread_stop();
+public:
+    //for others to get timer service
+    void add_timer(int64_t timeout, int64_t timerid, ITimerHandler* callback);
+    void delete_timer(int64_t timeout, int64_t timerid, ITimerHandler* callback);
+private:
+    void check_timeout();
+private:
+    bool thread_start_flag;
+    int64_t last_thread_time;
+    vector<timer_item> timer_vector;
+};
+
+#endif /* CORE_RS_TIMER_H_ */
